01de4f7382aed5ed6a4aa2472ed5c3d781f2cedd
Ahmet <Ahmet@IAMAHMETKOCA>
2023-05-11 19:30:18 +0300

    Add project files.

---
+++ rfid-with-login/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace rfid_with_login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.cbTapeSpeed = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblRfidUid = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(24, 19);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(150, 21);
+            this.cbPort.TabIndex = 0;
+            // 
+            // cbTapeSpeed
+            // 
+            this.cbTapeSpeed.FormattingEnabled = true;
+            this.cbTapeSpeed.Location = new System.Drawing.Point(24, 46);
+            this.cbTapeSpeed.Name = "cbTapeSpeed";
+            this.cbTapeSpeed.Size = new System.Drawing.Size(150, 21);
+            this.cbTapeSpeed.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(24, 73);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(72, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Coınnect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(102, 73);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(72, 23);
+            this.btnDisconnect.TabIndex = 3;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbPort);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbTapeSpeed);
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(202, 134);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // lblRfidUid
+            // 
+            this.lblRfidUid.AutoSize = true;
+            this.lblRfidUid.Location = new System.Drawing.Point(243, 61);
+            this.lblRfidUid.Name = "lblRfidUid";
+            this.lblRfidUid.Size = new System.Drawing.Size(35, 13);
+            this.lblRfidUid.TabIndex = 6;
+            this.lblRfidUid.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 162);
+            this.Controls.Add(this.lblRfidUid);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.ComboBox cbTapeSpeed;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblRfidUid;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ rfid-with-login/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace rfid_with_login
+{
+    public partial class Form1 : Form
+    {
+        string portname, tapespeed;
+        string[] ports = SerialPort.GetPortNames();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            portname = cbPort.Text;
+            tapespeed = cbTapeSpeed.Text;
+
+            try
+            {
+            serialPort1.PortName= portname;
+            serialPort1.BaudRate = Convert.ToInt32(tapespeed);
+
+            serialPort1.Open();
+
+            label1.Text = "Connected";
+            label1.ForeColor= Color.Green;
+            }
+            catch 
+            {
+                serialPort1.Close();
+                serialPort1.Open();
+                MessageBox.Show("Connection is already open.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+
+                label1.Text = "Disconnected";
+                label1.ForeColor = Color.Red;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string result;
+            result = serialPort1.ReadExisting();
+            if (result != "")
+            {
+                lblRfidUid.Text = result;
+                if (lblRfidUid.Text == "2172407421")
+                {
+                    Form2 fr = new Form2();
+                    fr.Show();
+                    this.Hide();
+                }
+                
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string port in ports)
+            {
+                cbPort.Items.Add(port);
+            }
+            cbTapeSpeed.Items.Add("2400");
+            cbTapeSpeed.Items.Add("4800");
+            cbTapeSpeed.Items.Add("9600");
+            cbTapeSpeed.Items.Add("19200");
+            cbTapeSpeed.Items.Add("115200");
+
+            cbPort.SelectedIndex = 0;
+            cbTapeSpeed.SelectedIndex = 2;
+        }
+    }
+}
