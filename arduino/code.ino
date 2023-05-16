d27942964ee00f249544efc159479afb28485ab3
Ahmet Koca <95000404+iamahmetkoca@users.noreply.github.com>
2023-05-16 10:59:15 +0300

    Create code.ino

---
+++ arduino/code.ino
@@ -0,0 +1,30 @@
+
+#include <MFRC522.h>
+#include <SPI.h>
+
+int rst_pin = 5;
+int ss_pin = 10;
+
+MFRC522 rfid(ss_pin, rst_pin);
+
+void setup() {
+  Serial.begin(9600); 
+  SPI.begin();     
+  rfid.PCD_Init();  
+}
+
+void loop() {
+  if(rfid.PICC_IsNewCardPresent())
+  {
+  if(rfid.PICC_ReadCardSerial())
+  {
+    Serial.print(rfid.uid.uidByte[0]);
+    Serial.print(rfid.uid.uidByte[1]);
+    Serial.print(rfid.uid.uidByte[2]);
+    Serial.print(rfid.uid.uidByte[3]);
+  }
+
+      rfid.PICC_HaltA();
+    } 
+
+  }
